a377a4574b72192382e65ba68c0dfde3eb8855c5
Owner <sam@jimmathews.net>
2023-07-16 17:52:10 +0100

    First commit, Letter mail merge changes

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,7 +1,7 @@
 #Create a letter using starting_letter.txt
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend". Test.
+#Save the letters in the folder "ReadyToSend". Test. Test2.
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
